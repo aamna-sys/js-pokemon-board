924c91566d7ca6da4d35f11bc40bc539e408bebf
aamna-sys <aamna.sayyed@systemsltd.com>
2021-11-12 10:52:17 +0500

    Add pokemons and styles

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const container = document.querySelector("#container");
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+//styles
+document.querySelector("h1").style.textAlign = "center";
+
+for (let i = 1; i <= 151; i++) {
+    const contentDiv = document.createElement("div");
+    const img = document.createElement("img");
+    img.src = `${baseURL}${i}.png`;
+    const span = document.createElement("span");
+    span.innerText = `#${i}`;
+
+    //styles
+    contentDiv.style.display = "inline-block";
+    img.style.display = "block";
+    contentDiv.style.textAlign = "center";
+
+    //append
+    contentDiv.append(img, span);
+    container.append(contentDiv);
+}
